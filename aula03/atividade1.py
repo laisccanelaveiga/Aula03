ace39c542b1d3779e6d9930942efbf41e826aec6
laisccanelaveiga <laisccveiga@gmail.com>
2026-05-04 21:21:52 -0300

    Corrigindo os códigos e resolvendo Atividade1

---
+++ aula03/atividade1.py
@@ -0,0 +1,10 @@
+#compra acima 250 aplicar 16% de desconto
+
+valor = float(input("Digite o valor da compra: "))
+
+if valor >= 250:
+    valor -= (valor * 0.16)
+    print(f'O valor à pagar após desconto é de R$ {valor:.2f}')
+
+else:
+    print(f"Valor à pagar: R$ {valor:.2f}")
